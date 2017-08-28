d4938c661204c32df54557cf04ec2fbe9a2116f8
Juan Manuel Arias <trasgofurioso@gmail.com>
2017-08-27 21:43:22 -0300

    Implementing task check

--- functions.php
+++ functions.php
@@ -15,4 +15,14 @@ function xpress_mvc_example_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'xpress_mvc_example_enqueue_styles' );
 
 require 'xpress/post-types/task.php';
-require 'xpress/controllers/tasks.php';
+require 'xpress/controllers/tasks-controller.php';
+
+function xpress_mvc_example_is_checked( $task ) {
+	return ! empty( get_post_meta( $task->ID, 'checked', true ) );
+}
+
+function xpress_mvc_example_update_post_meta( $post_id, $meta_key, $meta_value ) {
+	if ( ! add_post_meta( $post_id, $meta_key, $meta_value, true ) ) {
+	   update_post_meta( $post_id, $meta_key, $meta_value );
+	}
+}
