480b366b550024859934e0fb3ac2e64e5e1b8289
Juan Manuel Arias <trasgofurioso@gmail.com>
2017-12-01 23:48:06 -0200

    Implementing task model in controller and views

--- functions.php
+++ functions.php
@@ -15,14 +15,10 @@ function xpress_mvc_example_enqueue_styles() {
 add_action( 'wp_enqueue_scripts', 'xpress_mvc_example_enqueue_styles' );
 
 require 'xpress/post-types/task.php';
+require 'xpress/models/task-model.php';
 require 'xpress/controllers/tasks-controller.php';
 
 function xpress_mvc_example_is_checked( $task ) {
-	return ! empty( get_post_meta( $task->ID, 'checked', true ) );
-}
-
-function xpress_mvc_example_update_post_meta( $post_id, $meta_key, $meta_value ) {
-	if ( ! add_post_meta( $post_id, $meta_key, $meta_value, true ) ) {
-	   update_post_meta( $post_id, $meta_key, $meta_value );
-	}
+	$id = is_a( $task, 'Task_Model' ) ? $task->id : $task->ID;
+	return get_post_meta( $id, 'completed', true ) === 'true';
 }
