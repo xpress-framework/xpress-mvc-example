fa932f9a7735635800d4c10d1c5557e881f2389e
Juan Manuel Arias <trasgofurioso@gmail.com>
2017-12-02 00:44:25 -0200

    Adding line breaks

--- xpress/templates/edit-task.php
+++ xpress/templates/edit-task.php
@@ -15,11 +15,15 @@ global $xpress_viewmodel;
 			</header><!-- .page-header -->
 
 			<article>
+
 				<form action="<?php echo esc_url( xpress_mvc_get_route_permalink( 'update-task', array( 'slug' => $xpress_viewmodel->slug ) ) );?>" method="POST" class="task type-task hentry">
+
 					<div class="entry-content">
+
 						<input type="hidden" name="_method" value="PATCH">
 						<input type="hidden" name="completed" value="<?php echo xpress_mvc_example_is_checked( $xpress_viewmodel ) ? 'true' : 'false';?>">
 						<input type="hidden" name="id" value="<?php esc_attr_e( $xpress_viewmodel->id );?>">
+
 						<input type="text" name="title" placeholder="Enter task here..." value="<?php esc_attr_e( $xpress_viewmodel->title );?>">
 
 						<?php get_template_part( 'xpress/templates/partials/form-errors', 'task' ); ?>
@@ -29,7 +33,9 @@ global $xpress_viewmodel;
 					<footer class="entry-footer">
 						<button type="submit">Save</button>
 					</footer><!-- .entry-footer -->
+
 				</form>
+
 			</article><!-- #post-## -->
 		</main><!-- .site-main -->
 	</div><!-- .content-area -->
